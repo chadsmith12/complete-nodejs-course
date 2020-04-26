fbba10ae528e246e15b07463f5da2569677db935
Chad Smith <smith.chad12@gmail.com>
2020-04-26 01:17:24 -0500

    add basic command line parsing with yargs to notes app

--- node-module-system-notes-app/app.js
+++ node-module-system-notes-app/app.js
@@ -1,5 +1,36 @@
-const validator = require('validator');
-const getNotes = require('./notes');
+const chalk = require('chalk');
+const yargs = require('yargs');
 
-console.log(validator.isURL('https:mead.io'));
-console.log(getNotes());
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => {
+        console.log(chalk.green("Adding a new note!"));
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log(chalk.blue("Removing a note!"));
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: () => {
+        console.log(chalk.blue("Listing your note!"));
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read your notes',
+    handler: () => {
+        console.log(chalk.blue("Reading your note!"));
+    }
+})
+
+console.log(yargs.argv);
