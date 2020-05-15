257aeab91ee55a14e105a8b45f06088a5f310c14
Chad Smith <smith.chad12@gmail.com>
2020-05-14 20:59:52 -0500

    add hbs templating engine to express

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -5,9 +5,31 @@ const staticAssets = path.join(__dirname, '../static');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(staticAssets));
 
 // routes
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Chad Smith'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Chad Smith'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help!',
+        msg: 'Use this application to look for the weather.'
+    })
+})
+
 app.get('/weather', (req, res) => {
     res.send({
         location: 'Arlington, Texas',
