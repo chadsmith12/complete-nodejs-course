2f4706c3949116bc838ebe75b098820dda2a0b60
Chad Smith <smith.chad12@gmail.com>
2020-07-19 00:02:37 -0500

    add fetch call to get weather information

--- webserver/static/scripts/app.js
+++ webserver/static/scripts/app.js
@@ -1,1 +1,13 @@
 console.log("Client side javascript file is loaded!");
+
+const url = '/weather?address=Boston';
+fetch(url).then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            console.log(data.error);
+        }
+        else {
+            console.log(data.location, data.forecast);
+        }
+    })
+});
