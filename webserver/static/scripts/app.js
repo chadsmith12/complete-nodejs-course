eb27aff32863573723bdfa389de136c80de6c47f
Chad Smith <smith.chad12@gmail.com>
2020-07-19 00:29:50 -0500

    add location search form to  get location

--- webserver/static/scripts/app.js
+++ webserver/static/scripts/app.js
@@ -1,13 +1,36 @@
 console.log("Client side javascript file is loaded!");
 
-const url = '/weather?address=Boston';
-fetch(url).then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error);
+const locationForm = document.querySelector('#locationForm');
+const searchText = document.querySelector('#search');
+
+
+const getWeather = (searchLocation) => {
+    const url = `/weather?address=${searchLocation}`;
+    return new Promise((resolve, reject) => {
+        fetch(url).then((response) => {
+            response.json().then((data) => {
+                resolve(data);
+            })
+        }).catch((error) =>{
+            reject(error);
+        });
+    });
+}
+
+
+locationForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = searchText.value;
+    if(!location) {
+        console.log('Please enter a location');
+        return false;
+    }
+    getWeather(location).then((data) => {
+        if(data.error)  {
+            console.log(data.error)
+        } else  {
+            console.log(data.forecast)
+            console.log(data.location);
         }
-        else {
-            console.log(data.location, data.forecast);
-        }
-    })
+    });
 });
